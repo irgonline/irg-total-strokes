93d505b2a156f97ec3c90c97eae5bb6b9abe17a8
Henry Chan <hfhchan@users.noreply.github.com>
2023-04-30 02:18:38 +0800

    Regenerate data with source code separation

--- generate-total-strokes.mjs
+++ generate-total-strokes.mjs
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const rows = fs
+const rows1 = fs
   .readFileSync("data/totalstrokes.txt", "utf-8")
   .split("\r\n")
   .filter((o) => o)
@@ -9,15 +9,35 @@ const rows = fs
     return parseInt(codepoint.slice(2), 16) >= 0x3400;
   });
 
+const strokeCounts1 = new Map(
+  rows1.map((o) => {
+    const [codepoint, tsfs] = o.split(" ");
+    const [ts] = tsfs.split("|");
+    return [codepoint, ts];
+  })
+);
+
+const rows2 = fs
+  .readFileSync("data/source-code-separation.txt", "utf-8")
+  .split("\n")
+  .filter((o) => o)
+  .filter((o) => !o.startsWith("#"));
+
+rows2.forEach((o) => {
+  const [codepoint, char, ts] = o.split("\t");
+
+  const currentTs = strokeCounts1.get(codepoint);
+  if (currentTs && currentTs !== ts) {
+    console.warn(`${codepoint}: Expected stroke count ${ts}, got ${currentTs}`);
+  }
+  strokeCounts1.set(codepoint, ts);
+});
+
 fs.writeFileSync(
   "kIRGTotalStrokes.txt",
-  rows
-    .map((o) => {
-      const [codepoint, tsfs] = o.split(" ");
-      const [ts] = tsfs.split("|");
-      return `${codepoint}\tkIRGTotalStrokes\t${ts}`;
-    })
-    .join("\n")
+  Array.from(strokeCounts1, ([codepoint, ts]) => {
+    return `${codepoint}\tkIRGTotalStrokes\t${ts}`;
+  }).join("\n") + "\n"
 );
 
 // Generate diff
@@ -32,21 +52,17 @@ const existingTotalStrokes = new Map(
     })
 );
 
-const diffRows = rows
-  .map((o) => {
-    const [codepoint, tsfs] = o.split(" ");
-    const [ts, fs] = tsfs.split("|");
-    const existingTs = existingTotalStrokes.get(codepoint);
-    if (existingTs !== ts) {
-      return `${codepoint}\t${String.fromCodePoint(
-        parseInt(codepoint.slice(2), 16)
-      )}\t${existingTs}\t${ts}`;
-    }
-    return null;
-  })
-  .filter((o) => o);
+const diffRows = Array.from(strokeCounts1, ([codepoint, ts]) => {
+  const existingTs = existingTotalStrokes.get(codepoint);
+  if (existingTs !== ts) {
+    return `${codepoint}\t${String.fromCodePoint(
+      parseInt(codepoint.slice(2), 16)
+    )}\t${existingTs}\t${ts}`;
+  }
+  return null;
+}).filter((o) => o);
 
 fs.writeFileSync(
   "kIRGTotalStrokesComparison.tsv",
-  "codepoint\tchar\texisting value\tnew value\n" + diffRows.join("\n")
+  "codepoint\tchar\texisting value\tnew value\n" + diffRows.join("\n") + "\n"
 );
